7e56c29f099812cec7c532e062a78c64ba45af59
Victor Bobkov <victor@Victors-MacBook-Pro.local>
2016-08-12 10:46:46 -0700

    Ex Nihilo

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+print_r('<div style="color:red">D15 15 13|_|7 4 7357 |v|4y|\|3</div>');
+print_r('<div style="color:red">3y3 4|v| 1337 124><012</div>');
+print_r('<div style="color:red">P|-|3312 m41 1337n355</div>');
+print_r('<pre>');
+print_r($_POST);
+print_r('</pre>');
+
+?>
