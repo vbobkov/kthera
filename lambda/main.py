68acd50cda332a901c1d8db5ea8d83115ed1e744
vic <vik.bobkov@gmail.com>
2016-08-13 19:46:34 -0700

    lambda python s3 is a go - but, the lambda call itself is not non-blocking - must figure out a way to do concurrency and multithreading

--- lambda/main.py
+++ lambda/main.py
@@ -6,27 +6,20 @@ import boto3
 import json
 #import request
 import sys
+import time
 import urllib
 
-print('Loading function')
+print('Initializing K\'Thera')
 
+# s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
-    # print("Received event: " + json.dumps(event, indent=2))
-    # print("value1 = " + event['key1'])
-    # print("value2 = " + event['key2'])
-    # print("value3 = " + event['key3'])
-    # return event['key1']  # Echo back the first key value
-    # raise Exception('Something went wrong')
-
-    # print(event);
-    # print(context);
-    # print(request);
-
 	# print('{"wtf":"srs"}');
 	data = {}
 	data['event'] = event
 	# data['context'] = context
+	# print(data)
 
 	# json_data = json.dumps(data)
 	# print('Content-type: text/html\n\n')
@@ -34,4 +27,23 @@ def lambda_handler(event, context):
 	# print(json_data)
 	# return json_data
 
+	log_filename = '1337.txt'
+
+	data['s3_log'] = upload_to_s3('kthera', 'beeswax_archive/' + log_filename, 'DANKALICIOUS!!!!oneone\nSIKK NO SCOPEZ M9');
+
 	return data
+
+def upload_to_s3(bucket_name, filepath, content):
+	result = False;
+	# TO DO: figure out why the hell the lambda endpoint is blocking (can only execute one at a time)
+	# time.sleep(10)
+	try:
+		# http://boto3.readthedocs.io/en/latest/reference/services/s3.html
+		s3.Bucket(bucket_name).put_object(ACL = 'private', Body = content, Key = filepath)
+		result = True;
+	except Exception as e:
+		print(e)
+		# print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+		print('Error setting object {} in bucket {}.'.format(filepath, bucket_name))
+		raise e
+	return result
