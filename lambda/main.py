fcd28dc07c809cb67002ce237dcbd54f1250c33c
vic <vik.bobkov@gmail.com>
2016-08-13 17:55:28 -0700

    python passes get/post data through, now to figure out how to store in s3

--- lambda/main.py
+++ lambda/main.py
@@ -1,8 +1,12 @@
 from __future__ import print_function
 # from flask import request
+
+import boto3
+#import flask
 import json
-import requests
+#import request
 import sys
+import urllib
 
 print('Loading function')
 
@@ -14,6 +18,20 @@ def lambda_handler(event, context):
     # print("value3 = " + event['key3'])
     # return event['key1']  # Echo back the first key value
     # raise Exception('Something went wrong')
-    print(event);
-    print(context);
-    print(request);
+
+    # print(event);
+    # print(context);
+    # print(request);
+
+	# print('{"wtf":"srs"}');
+	data = {}
+	data['event'] = event
+	# data['context'] = context
+
+	# json_data = json.dumps(data)
+	# print('Content-type: text/html\n\n')
+	# print('Content-type: application/json\n\n')
+	# print(json_data)
+	# return json_data
+
+	return data
