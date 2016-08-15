2644f0fa50de2e8885e49a199e8f1dffac5bbb43
vic <vik.bobkov@gmail.com>
2016-08-14 21:39:39 -0700

    dynamodb works - put_item (single item), batch insert probably works too, but unnecessary right now

--- lambda/main.py
+++ lambda/main.py
@@ -13,7 +13,8 @@ print('Initializing K\'Thera')
 
 # s3 = boto3.client('s3')
 # s3 = boto3.resource('s3')
-firehose = boto3.client('firehose')
+# firehose = boto3.client('firehose')
+dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
 	# print('{"wtf":"srs"}');
@@ -28,22 +29,41 @@ def lambda_handler(event, context):
 	# print(json_data)
 	# return json_data
 
-	log_filename = '1337.txt'
-
+	# log_filename = '1337.txt'
 	# data['s3_upload_results'] = upload_to_s3('kthera', 'beeswax_archive/' + log_filename, 'DANKALICIOUS!!!!oneone\nSIKK NO SCOPEZ M9');
-	data['firehose_upload_results'] = upload_to_firehose(
-		'kthera',
-		[
-			{
-				'Data': 'DANKALICIOUS!!!!oneone\nSIKK NO SCOPEZ M9\n'
-			},
-			{
-				'Data': 'DA SIKKEST 720NOSCOPE!!!11oneone\n420BLAZEIT\n\n'
-			}
-		]
-	);
+	# data['firehose_upload_results'] = upload_to_firehose(
+		# 'kthera',
+		# [
+			# {
+				# 'Data': 'DANKALICIOUS!!!!oneone\nSIKK NO SCOPEZ M9\n'
+			# },
+			# {
+				# 'Data': 'DA SIKKEST 720NOSCOPE!!!11oneone\n420BLAZEIT\n\n'
+			# }
+		# ]
+	# )
+
+	data['dynamodb_results'] = insert_into_dynamodb(
+		'kthera_beeswax_feed',
+		{
+			'request_id': {'N': '1337'},
+			'json': {'S': '{"uwot": "m8??"}'}
+		}
+	)
 
 	return data
+
+def insert_into_dynamodb(table_name, data):
+	result = False;
+	try:
+		# http://boto3.readthedocs.io/en/latest/reference/services/dynamodb.html
+		response = dynamodb.put_item(TableName = table_name, Item = data)
+		# response = dynamodb.batch_write_item(TableName = table_name, RequestItems = data)
+		result = True;
+	except Exception as e:
+		print(e)
+		raise e
+	return result;
 
 def upload_to_s3(bucket_name, filepath, data):
 	result = False;
